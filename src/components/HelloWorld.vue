77805eb7f05d7b10730ade19c1e98159d117e26b
rensa_capo <rensacoltd@gmail.com>
2021-02-11 18:30:11 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,67 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p><button v-on:click="getAPI()">Next Product</button></p>
+    <p>{{ record.title }}</p>
+    <img v-bind:src=record.url>
+    <div v-html=record.html></div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios from 'axios'
+
+interface Record {
+  title: string;
+  year: number;
+  url: string;
+  html: string;
+  count: number;
+}
 
 export default defineComponent({
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  data() {
+    return{
+      record: {
+        title: 'jas',
+        year: 2020,
+        url: 'https://cdn.shopify.com/s/files/1/0415/0791/3886/products/877_01.jpg?v=1612667135',
+        html: '<meta charset="utf-8">\n' +
+              '<p data-mce-fragment="1"><span data-mce-fragment="1">●Artist: Carmen McRae</span></p>\n' +
+              `<p data-mce-fragment="1"><span data-mce-fragment="1">●Title: Recorded Live At Bubba's</span></p>\n` +
+              '<p data-mce-fragment="1"><span data-mce-fragment="1">●Label: Baybridge Records (Japan)</span></p>\n' +
+              '<p data-mce-fragment="1"><span data-mce-fragment="1">●Number: KUX-163-B</span></p>\n' +
+              '<p data-mce-fragment="1"><span data-mce-fragment="1">●Format: LP</span></p>\n' +
+              '<p data-mce-fragment="1"><span data-mce-fragment="1">●Year: 1981</span></p>\n' +
+              '<p data-mce-fragment="1"><span data-mce-fragment="1">●Recording Date: 1981</span></p>\n' +
+              `<p data-mce-fragment="1"><span data-mce-fragment="1">●Track List: A1. Black Magic A2. Superwoman A3. New York State Of Mind A4. Underneath The Apple Tree A5. Thou Swell A6. Send In The Clowns B1. I Just Can't Wait B2. How Long B3. If I Were A Bell B4. My Foolish Heart B5. Secret Love</span></p>\n` +
+              '<p data-mce-fragment="1"><span data-mce-fragment="1">●Condition: Used (Sleeve: EX-. with insert and OBI. some light damage on cover. Vinyl: EX-. some light marks. but good shape.)</span><br data-mce-fragment="1"><br data-mce-fragment="1"><span data-mce-fragment="1">-Grading Policy-</span><br data-mce-fragment="1"><span data-mce-fragment="1">M～NM～EX+～EX～EX-～VG+～VG～VG- (8 grades)</span><br data-mce-fragment="1"><br data-mce-fragment="1"><span data-mce-fragment="1">M to EX → Clean copy. </span><br data-mce-fragment="1"><br data-mce-fragment="1"><span data-mce-fragment="1">EX-, VG+ → Some (light) wear and marks. but good shape. </span><br data-mce-fragment="1"><br data-mce-fragment="1"><span data-mce-fragment="1">VG, VG- → Bad condition. some wear and marks.</span><br data-mce-fragment="1"><br data-mce-fragment="1"><span data-mce-fragment="1">You can order by e-mail. Please contact to solidityrecords@gmail.com</span></p>\n' +
+              '\n' +
+              '<iframe width="560" height="315" src="https://www.youtube.com/embed/4as30GSYIxo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        count: 1
+      } as Record
+    }
+  },
+  methods:{
+    getAPI(){
+      const num = this.record.count += 1
+
+      const url = '/api?num=' + num.toString();
+      console.log('url', url);
+
+      axios.get(url).then((response) => {
+        console.log('response', response.data.Title);
+        if ( response.data['Title'] !== '' ){
+          this.record.title = response.data['Title'];
+          this.record.html = response.data['Body (HTML)'];
+        }
+        this.record.url = response.data['Image Src'];
+      });
+
+    },
+  },
+  computed:{
   },
 });
 </script>
@@ -58,5 +81,8 @@ li {
 }
 a {
   color: #42b983;
+}
+img {
+  zoom: 13%;
 }
 </style>
