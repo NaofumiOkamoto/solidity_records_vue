4d67d96a2e71f2bfa8e8482e31ce49a5e86445fd
NaofumiOkamoto <n.okamooo@gmail.com>
2022-01-23 11:36:27 +0900

    update updateproduct

--- src/components/admin/Product.vue
+++ src/components/admin/Product.vue
@@ -6,15 +6,13 @@
     <div style="display:flex;">
       <div style="width: 20%; height: 10px;"></div>
       <div class="admin_product_page_box">
-        <!-- {{ getProduct.products[0]["artist"] }} -->
         <el-icon><arrow-left /></el-icon>
         <h3 v-if="paramsItemId !== 'new'">{{ artist}} - {{title}}</h3>
         <h3 v-if="paramsItemId === 'new'">add product</h3>
         <div class="admin_product_edit_box">
           <p v-if="paramsItemId === 'new'">item_id <br> <input class="" type="text" v-model="itemId" placeholder=""></p>
           <p v-else>item_id<br> <span class="" >{{paramsItemId}}</span></p>
-          <p v-if="paramsItemId === 'new'">master_id <br> <input class="" type="text" v-model="master_id" placeholder=""></p>
-          <p v-else>master_id<br> <span class="" >{{masterId}}</span></p>
+          <p>master_id <br> <input class="" type="text" v-model="masterId" placeholder=""></p>
           <p>artist        <br> <input class="" type="text" v-model="artist" placeholder=""></p>
           <p>title         <br> <input class="" type="text" v-model="title" placeholder=""></p>
           <p>label         <br> <input class="" type="text" v-model="label" placeholder=""></p>
@@ -64,20 +62,13 @@ export default defineComponent({
     this.getProduct()
     console.log(this.paramsItemId)
   },
-  // computed: {
-  //   getProduct(){
-  //     const store = useStore(key)
-  //     store.dispatch('getProducts', 'where item_id = ' + this.itemId)
-  //     this.product = "aaa"
-  //     return store.state
-  //   },
-  // },
   methods: {
     getProduct() {
       if(this.paramsItemId !== 'new'){
         const url = '/getApi?sql= where item_id = ' + this.paramsItemId;
         console.log('getApi url', url)
         axios.get(url).then((response) => {
+          this.itemId = response.data[0]["item_id"]
           this.masterId = response.data[0]["master_id"]
           this.artist = response.data[0]["artist"]
           this.title = response.data[0]["title"]
@@ -91,48 +82,38 @@ export default defineComponent({
     },
     updateProduct() {
       if(this.paramsItemId !== 'new'){
-        let url = '/updateProduct?sql= '
-        url += 'artist = "' + this.artist + '", '
-        url += 'title = "' + this.title + '", '
-        url += 'label = "' + this.label + '", '
-        url += 'number = "' + this.number + '", '
-        url += 'format = "' + this.format + '", '
-        url += 'release_year = "' + this.releaseYear + '", '
-        url += 'recoding_date = "' + this.recodingDate 
-        url += '" where item_id = ' + this.paramsItemId;
-        console.log('url', url)
+        const url = '/updateProduct?sql= ' + this.createUrl()
         axios.get(url).then((response) => {
-          console.log(response.status)
+          if (response.status === 200) {
+            alert("success!!")
+          }
+        })
+      } else {
+        const url = '/createProduct?sql= ' + this.createUrl()
+        axios.get(url).then((response) => {
           if (response.status === 200) {
             alert("success!!")
           }
         })
       }
     },
-    createProduct() {
-      if(this.paramsItemId === 'new'){
-        let url = '/updateProduct?sql= '
-        url += 'artist = "' + this.artist + '", '
-        url += 'title = "' + this.title + '", '
-        url += 'label = "' + this.label + '", '
-        url += 'number = "' + this.number + '", '
-        url += 'format = "' + this.format + '", '
-        url += 'release_year = "' + this.releaseYear + '", '
-        url += 'recoding_date = "' + this.recodingDate 
-        url += '" where item_id = ' + this.paramsItemId;
-        console.log('url', url)
-        axios.get(url).then((response) => {
-          console.log(response.status)
-          if (response.status === 200) {
-            alert("success!!")
-          }
-        })
-      }
+    createUrl() {
+      let url = this.itemId !== ''      ? 'item_id = "'      + this.itemId + '", '      : 'item_id = NULL, '
+      url += this.masterId !== ''    ? 'master_id = "'    + this.masterId + '", '    : 'master_id = NULL, '
+      url += this.artist !== ''      ? 'artist = "'       + this.artist + '", '      : 'artist = NULL, '
+      url += this.title !== ''       ? 'title = "'        + this.title + '", '       : 'title = NULL, '
+      url += this.label !== ''       ? 'label = "'        + this.label + '", '       : 'label = NULL, '
+      url += this.number !== ''      ? 'number = "'       + this.number + '", '      : 'number = NULL, '
+      url += this.format !== ''      ? 'format = "'       + this.format + '", '      : 'format = NULL, '
+      url += this.releaseYear !== '' ? 'release_year = "' + this.releaseYear + '", ' : 'release_year = NULL, '
+      url += this.recodingDate !== '' ? 'recoding_date = "' + this.recodingDate + '"' : 'recoding_date = NULL, '
+      url += ' where item_id = ' + this.paramsItemId;
+      return url
     }
+
   }
 });
 </script>
-
 <style scoped>
 .admin_product_page {
   background-color: #f0efef;
