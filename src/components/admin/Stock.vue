f633d4734259aebff047cd570f41fff9d70a5f3c
NaofumiOkamoto <n.okamooo@gmail.com>
2022-02-20 12:23:12 +0900

    update validate

--- src/components/admin/Stock.vue
+++ src/components/admin/Stock.vue
@@ -10,8 +10,8 @@
         <h1>ジャンル</h1>
         <table class="">
           <tr>
-            <th>sort_num</th>
-            <th>id</th>
+            <th class="column_num">sort<br>num</th>
+            <th class="column_num">id</th>
             <th>main</th>
             <th>sub</th>
             <th></th>
@@ -30,14 +30,29 @@
             <td v-if="editGenre === genre.id" class="column_text"><input class="genre_input_text" v-model="genre.sub"></td>
             <td v-if="editGenre === genre.id" class="column"><button @click="update(genre)">update</button></td>
           </tr>
+          <tr class="search_result">
             <!-- 追加時 -->
-            <td v-if="isAddGenre" class="column_num"> <input class="genre_input_num" @blur="validateNum('sort_num')" v-model="addGenre.sort_num" ></td>
-            <td v-if="isAddGenre" class="column_num"> <input class="genre_input_num" @blur="validateNum('id')" v-model="addGenre.id" id="input_id" ></td>
-            <td v-if="isAddGenre" class="column_text"><input class="genre_input_text" v-model="addGenre.main" ></td>
-            <td v-if="isAddGenre" class="column_text"><input class="genre_input_text" v-model="addGenre.sub" ></td>
+            <td v-if="isAddGenre" class="column_num"> 
+              <input class="genre_input_num" @blur="validateNum($event, 'sort_num')" v-model="addGenre.sort_num" >
+            </td>
+            <td v-if="isAddGenre" class="column_num">
+              <input class="genre_input_num" @blur="validateNum($event, 'id')" v-model="addGenre.id" id="input_id" >
+            </td>
+            <td v-if="isAddGenre" class="column_text">
+              <input class="genre_input_text" @blur="validateStr($event, 'main')" v-model="addGenre.main" >
+            </td>
+            <td v-if="isAddGenre" class="column_text">
+              <input class="genre_input_text" @blur="validateStr($event, 'main')" v-model="addGenre.sub" >
+            </td>
             <!-- ToDo: 追加 -->
-            <td v-if="isAddGenre" class="column"><button @click="addGenreFunc(addGenre)">add</button></td>
-            <el-button @click="isAddGenre = true" type="primary" plain>Primary</el-button>
+            <td v-if="isAddGenre" class="column">
+              <button @click="addGenreFunc(addGenre)">add</button>
+              <button @click="addGenreFunc(addGenre)">cancel</button>
+            </td>
+          </tr>
+            <el-button v-if="!isAddGenre" @click="isAddGenre = true" type="primary" plain>Primary</el-button>
+            <span id="tooltips" class="tooltips">数字で入力してください</span>
+            <span id="tooltips_str" class="tooltips">文字を入力してください</span>
         </table>
       </div>
     </div>
@@ -54,12 +69,14 @@ export default defineComponent({
       editGenre: number;
       addGenre: {[key: string]: string};
       isAddGenre: boolean;
+      isValid: boolean;
   } {
     return {
       genres: [{}],
       editGenre: 0,
       addGenre: {'sort_num': '', 'id': '', 'main': '', 'sub': ''},
       isAddGenre: false,
+      isValid: false
     }
   },
   created() {
@@ -77,8 +94,6 @@ export default defineComponent({
     addGenreFunc(addGenre: object) {
       console.log(addGenre)
       // ToDo: genreのinsert処理
-      // if (this.genres.indexOf(this.addGenre) === -1 && this.addGenre !== '') {
-
     },
     edit(genreId: number) {
       this.editGenre = genreId
@@ -96,12 +111,32 @@ export default defineComponent({
         this.getAllGenre()
       })
     },
-    validateNum(key: string) {
-      if (!(/[0-9]/).test(this.addGenre[key])) {
-        alert(key + " は数字だろぉぉぉぉぉぉぉ")
-        const el = document.getElementById('input_id')
+    validateNum(event: any, key: string) {
+      if ((/[0-9]/).test(this.addGenre[key])) { // OKの時
+        event.target.style.backgroundColor = "#fff"
+        this.isValid = true
+      } else {// NGの場合
+        event.target.style.backgroundColor = "#fb94e58c" // formの色変え
+        const el = document.getElementById('tooltips')
         if (el) {
-          el.style.backgroundColor = "#fb94e58c"
+          el.style.display = 'block'
+          event.target.parentElement.insertBefore(el, event.target)
+          setTimeout(() => { el.style.display = 'none'}, 3000);
+        }
+      }
+    },
+    validateStr(event: any, key: string) {
+      if (!(/$^/).test(this.addGenre[key])) {
+        event.target.style.backgroundColor = "#fff"
+        this.isValid = true
+        // Todo: このバリデーション判断どうするか
+      } else {
+        event.target.style.backgroundColor = "#fb94e58c" // formの色変え
+        const el = document.getElementById('tooltips_str')
+        if (el) {
+          el.style.display = 'block'
+          event.target.parentElement.insertBefore(el, event.target)
+          setTimeout(() => { el.style.display = 'none'}, 3000);
         }
       }
     }
@@ -114,17 +149,30 @@ tr {
   text-align: left;
 }
 th {
-  width: 200px;
+  width: 10%;
   text-align: center;
 }
 tr:hover {
   background-color: #e9eef3;
 }
+.column_num, .column_text {
+  position: relative;
+}
 .genre_input_num ,.genre_input_num{ 
   width: 100%;
 }
-.column_num {
-  width: 10%
+.tooltips {
+  position: absolute;
+  top: -50%;
+  background-color: #ff0000;
+  color: #fff;
+  border-radius: 10px;
+  padding: 10px;
+  z-index: 2;
+  width: 200px;
+  display: none;
+  font-size: 80%;
+  opacity: 50%;
 }
 .column_text {
   width: 40%
